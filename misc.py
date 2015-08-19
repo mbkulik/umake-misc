4eb4269f6a58cc0de8589c043460e2e4aa991f0b
Michael B. Kulik <mike.b.kulik@gmail.com>
2015-08-18 21:04:29 -0400

    fix indentation, description, and remove use of custom Decompressor

--- misc.py
+++ misc.py
@@ -18,7 +18,6 @@ import umake.frameworks.baseinstaller
 from bs4 import BeautifulSoup
 import os, urllib, logging, shutil
 from os.path import join, isfile
-from decompressor import Decompressor
 from umake.interactions import DisplayMessage, UnknownProgress
 from umake.network.download_center import DownloadCenter, DownloadItem
 from umake.tools import create_launcher, get_application_desktop_file,\
@@ -38,7 +37,7 @@ class Popcorntime(umake.frameworks.baseinstaller.BaseInstaller):
 
     def __init__(self, category):
         super().__init__(name="Popcorntime",
-                         description="Popcorntime torrent video player",
+                         description="Popcorntime video player",
                          category=category,
                          download_page=None,
                          only_on_archs=['amd64'],
@@ -95,7 +94,7 @@ class Drjava(umake.frameworks.baseinstaller.BaseInstaller):
 
 
     def download_provider_page(self):
-                download_url= "http://downloads.sourceforge.net/project/drjava/1.%20DrJava%20Stable%20Releases/drjava-stable-20140826-r5761/drjava-stable-20140826-r5761.jar?r=&ts=1439498832&use_mirror=iweb"
+        download_url= "http://downloads.sourceforge.net/project/drjava/1.%20DrJava%20Stable%20Releases/drjava-stable-20140826-r5761/drjava-stable-20140826-r5761.jar?r=&ts=1439498832&use_mirror=iweb"
         self.download_requests.append(DownloadItem(download_url))
         self.start_download_and_install()
 
